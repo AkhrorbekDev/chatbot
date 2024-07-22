bf73422daee532960cb44a02f43fc9cf87ff5490
axrorbek <axrorbeknis@icloud.com>
2024-07-22 17:07:58 +0500

    Update vite.config.js and index.html
    Commented out the rollupOptions in vite.config.js to disable externalizing Vue to allow Vite to handle it. In index.html, replaced local stylesheets and scripts with external links from CDNs and a Vercel app. This change is intended to handle dependencies more efficiently.

--- vite.config.js
+++ vite.config.js
@@ -22,17 +22,17 @@ export default defineConfig({
             fileName: 'my-lib',
             entry: resolve(__dirname, 'src/main.js'),
         },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['vue'],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: 'Vue',
-                },
-            },
-        },
+        // rollupOptions: {
+        //     // make sure to externalize deps that shouldn't be bundled
+        //     // into your library
+        //     external: ['vue'],
+        //     output: {
+        //         // Provide global variables to use in the UMD build
+        //         // for externalized deps
+        //         globals: {
+        //             vue: 'Vue',
+        //         },
+        //     },
+        // },
     },
 })
