41d9dd8c0e709fb6bca45bddd515837f32f3e573
axrorbek <axrorbeknis@icloud.com>
2024-07-23 10:16:38 +0500

    test f

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 // Instead of: import { createApp } from 'vue';
-const {createApp} = Vue;
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+export { App }
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import {fileURLToPath, URL} from 'node:url'
-
-import {resolve} from 'node:path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import externalGlobals from 'vite-plugin-external-globals';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue(),
-        externalGlobals({
-            vue: 'Vue',
-            // Add other dependencies here if needed
-        })
+        vue()
     ],
     resolve: {
         alias: {
@@ -20,23 +15,17 @@ export default defineConfig({
     },
     build: {
         lib: {
-            // Could also be a dictionary or array of multiple entry points
-            name: 'MyLib',
-            // the proper extensions will be added
-            fileName: 'my-lib',
-            entry: resolve(__dirname, 'src/main.js'),
+            entry: './src/main.js',
+            name: 'ChatWidget',
+            fileName: (format) => `chat-widget.${format}.js`
         },
-        // rollupOptions: {
-        //     // make sure to externalize deps that shouldn't be bundled
-        //     // into your library
-        //     external: ['vue'],
-        //     output: {
-        //         // Provide global variables to use in the UMD build
-        //         // for externalized deps
-        //         globals: {
-        //             vue: 'Vue',
-        //         },
-        //     },
-        // },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
     },
 })
