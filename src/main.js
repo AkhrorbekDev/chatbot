6a354f4c6842cc7867a4abcb9b0aa08fb5d5afba
axrorbek <axrorbeknis@icloud.com>
2024-07-31 12:23:28 +0500

    WIP4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/scss/main.scss'
-import App from './App.vue'
+import App from './components/App.vue'
 import {vMaska} from "maska/vue"
 import createAuth from "@/modules/auth";
 import Storage from '@/modules/storage';
